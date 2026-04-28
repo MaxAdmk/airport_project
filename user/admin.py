94b14860e3b229402c8e0dda8c22cbb2c4d28e02
MaxAdmk <maksimkeny@gmail.com>
2026-04-28 17:24:27 +0300

    feat: create CustomUserManager and switch User model to email authentication

--- user/admin.py
+++ user/admin.py
@@ -4,10 +4,18 @@ from .models import User
 
 @admin.register(User)
 class CustomUserAdmin(UserAdmin):
-    list_display = ('username', 'email', 'first_name', 'last_name', 'role', 'passport_code')
+    ordering = ('email',)
+    list_display = ('email', 'first_name', 'last_name', 'role', 'passport_code')
     list_filter = ('role', 'is_staff', 'is_active')
-    search_fields = ('username', 'first_name', 'last_name', 'email', 'passport_code')
+    search_fields = ('email', 'first_name', 'last_name', 'passport_code')
     
-    fieldsets = UserAdmin.fieldsets + (
-        ('Additional Info', {'fields': ('passport_code', 'role', 'citizenship', 'date_of_birth', 'phone_number')}),
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {
+            'fields': ('first_name', 'last_name', 'passport_code', 'citizenship', 'date_of_birth', 'phone_number')
+        }),
+        ('Permissions', {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'role', 'groups', 'user_permissions'),
+        }),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
     )
